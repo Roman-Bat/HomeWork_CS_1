d8454b8f961b11202f496ad85b4ffcc60621d3b0
Roman_Batrakov <kyct69@yandex.ru>
2023-09-03 10:38:47 +0300

    Выполнил задание №4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*********************************************************************************************************************
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+
+**********************************************************************************************************************/
+int num_N;
+int temp = 1;
+int[] mass;
+int index = 0;
+
+Console.WriteLine("Введите целое число: ");
+num_N = int.Parse(Console.ReadLine());
+mass = new int[num_N / 2];
+while (temp <= num_N)
+{
+   if (temp % 2 == 0)
+    {
+        mass[index] = temp;
+        index ++;
+    }
+    temp = temp + 1;
+} 
+   Console.WriteLine("Вы ввели: " + num_N);
+   Console.WriteLine("Все чётные числа от 1 до " + num_N);
+   foreach (var item in mass) 
+            {
+            Console.Write(" "+ item);
+            }
